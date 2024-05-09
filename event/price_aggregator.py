3db1014b3956b5920ac9c8d6e56a08a567894808
footprinthere <tai15515@kakao.com>
2024-05-09 15:59:51 +0900

    Add option to use already saved data files

--- event/price_aggregator.py
+++ event/price_aggregator.py
@@ -21,17 +21,44 @@ class PriceAggregator(Aggregator):
         self,
         *,
         event_times: list[datetime],
-        data_time_delta: timedelta,
+        data_files: Optional[list[str]] = None,
+        data_time_delta: Optional[timedelta] = None,
         event_window: tuple[int, int],
         estimation_window: tuple[int, int],
         result_file_format: Optional[str] = None,
-    ):
+    ) -> None:
         """
-        * `result_file_format` : str that includes a format `{}` to be replaced by the index of the event_time
+        * Either `data_files` or `data_time_delta` should be specified
+        * `result_file_format` : str that includes a format `{}` to be replaced by the index of the `event_time`
         """
         if result_file_format is not None:
             if "{}" not in result_file_format:
-                raise ValueError("result_file_format should include a format `{}`")
+                raise ValueError("`result_file_format` should include a format `{}`")
+
+        if data_files is not None:
+            print("Using saved dataset files")
+            for idx, (event_time, data_file) in enumerate(
+                tqdm(list(zip(event_times, data_files)))
+            ):
+                self.create_event(
+                    data_file=data_file,
+                    column_name=self._column_name,
+                    is_price=True,
+                    event_time=event_time,
+                    event_window=event_window,
+                    estimation_window=estimation_window,
+                    result_file=(
+                        None
+                        if result_file_format is None
+                        else result_file_format.format(idx)
+                    ),
+                )
+            return
+
+        if data_time_delta is None:
+            raise ValueError(
+                "Either `data_files` or `data_time_delta` should be specified"
+            )
 
         for idx, event_time in enumerate(tqdm(event_times)):
             self.create_price_event(
