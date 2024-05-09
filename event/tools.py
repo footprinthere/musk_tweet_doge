03d3da26cf8f38b0288b3b51d4bb58d1d810b71b
footprinthere <tai15515@kakao.com>
2024-05-09 14:55:11 +0900

    Fix conversion of data frame

--- event/tools.py
+++ event/tools.py
@@ -18,14 +18,16 @@ def single_import_returns(
     if date_format is not None:
         data["date"] = pd.to_datetime(data["date"], format=date_format)
 
-    if is_price:
-        for key in data.keys():
-            if key != "date":
-                if log_return:
-                    data[key] = np.diff(np.log(data[key]))
-                else:
-                    data[key] = np.diff(data[key]) / data[key][1:]
-            else:
-                data[key] = data[key][1:]  # remove the first date
+    if not is_price:
+        Single._save_parameter("returns", data)
+        return
 
-    Single._save_parameter("returns", data)
+    converted_data = data.iloc[1:]  # remove the first row
+    for key in data.keys():
+        if key != "date":
+            if log_return:
+                converted_data.loc[:, key] = np.diff(np.log(data[key]))
+            else:
+                converted_data.loc[:, key] = np.diff(data[key]) / data[key][1:]
+
+    Single._save_parameter("returns", converted_data)
