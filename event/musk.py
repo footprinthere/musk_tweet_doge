abc7fe66138671e11ee0df5c6fffd76dc3942df5
footprinthere <tai15515@kakao.com>
2024-06-03 20:38:53 +0900

    Mark special events

--- event/musk.py
+++ event/musk.py
@@ -9,12 +9,12 @@ MUSK_TWEET_TIMES = [
     datetime(2021, 2, 7, 23, 25, 0),  # 12
     datetime(2021, 2, 10, 16, 8, 0),  # 14
     datetime(2021, 2, 11, 10, 8, 0),  # 15
-    datetime(2021, 2, 15, 0, 25, 0),  # 16
+    datetime(2021, 2, 15, 0, 25, 0),  # 16 - requires 2-day data
     datetime(2021, 2, 21, 22, 27, 0),  # 18
     datetime(2021, 2, 24, 14, 0, 0),  # 19
     datetime(2021, 3, 1, 20, 57, 0),  # 20
     datetime(2021, 3, 6, 5, 40, 0),  # 22
-    datetime(2021, 3, 14, 0, 40, 0),  # 24
+    datetime(2021, 3, 14, 0, 40, 0),  # 24 - requires 2-day data
     datetime(2021, 4, 1, 12, 25, 0),  # 26
     datetime(2021, 4, 9, 9, 32, 0),  # 27
     datetime(2021, 4, 15, 5, 33, 0),  # 28 - Ante said "6:33:00", but looks like a typo
